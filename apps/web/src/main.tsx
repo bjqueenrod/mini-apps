d66d8df09ff00cd647d75f07dd20515fa3b16583
Guy Watson <guy@gwatson.com>
2026-04-09 04:16:02 +0700

    Square off tier pills slightly

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles.css?v=20260409f';
+import './styles.css?v=20260409g';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
