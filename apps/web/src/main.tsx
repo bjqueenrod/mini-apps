8e5f84ab843e0cbf3ecd7b3c703d3e2849729d91
Guy Watson <guy@gwatson.com>
2026-04-11 11:18:47 +0700

    Tighten pay button sizing

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles.css?v=20260411r';
+import './styles.css?v=20260411s';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
