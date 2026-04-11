c7f514ff0b6764c76b862a6c525996641e2e9ac6
Guy Watson <guy@gwatson.com>
2026-04-11 11:27:27 +0700

    Restore original pay button sizing

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles.css?v=20260411t';
+import './styles.css?v=20260411u';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
