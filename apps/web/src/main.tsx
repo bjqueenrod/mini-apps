71820defe98997f8abaddaccdd0fbae067f073cd
Guy Watson <guy@gwatson.com>
2026-04-11 10:44:36 +0700

    Tighten compact payment button padding

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles.css?v=20260411m';
+import './styles.css?v=20260411n';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
